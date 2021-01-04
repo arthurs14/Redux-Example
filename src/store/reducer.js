ed87d1aae2eff7f51e93c14436937689b9ee318a
Arthur Sandro <arthurcs426@gmail.com>
2021-01-03 16:33:39 -0800

    added an initial state to our reducer and fixed an issue due to outdated react

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+let initialState = {
+  persons: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
